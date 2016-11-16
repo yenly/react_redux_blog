29bdbc3425830f4bd87aea9327eed97fc6125e04
yenly <yencodes@gmail.com>
2016-11-16 02:54:56 -0800

    Add axios and redux-promise lib to setup API calls.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import promise from 'redux-promise'
 
 
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
