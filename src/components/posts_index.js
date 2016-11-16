7383d348291a50d75eaecc9756a9d9c3cee93325
yenly <yencodes@gmail.com>
2016-11-16 13:55:42 -0800

    Add life cycle method to call action creator to fetch posts.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// no longer needed when using the short cut below
+// import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of blog post.</div>;
+class PostsIndex extends Component {
+  // componentWillMount() is a lifecycle method
+  // only called once at first render of app, not later renders
+  componentWillMount() {
+    this.props.fetchPosts(); // calls our action creator
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchPosts }, dispatch);
+// }
+
+// export default connect(null, mapDispatchToProps)(PostsIndex);
+// short cut using es6 way to pass in object
+export default connect(null, { fetchPosts })(PostsIndex);
