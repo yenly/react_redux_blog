7ac924a578a6842712ac2aa0cfc95cb9eb301bea
yenly <yencodes@gmail.com>
2016-11-16 14:27:30 -0800

    Add create new post component and add link from main index.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 // no longer needed when using the short cut below
 // import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
   // componentWillMount() is a lifecycle method
@@ -13,7 +14,14 @@ class PostsIndex extends Component {
 
   render() {
     return (
-      <div>List of blog posts</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        List of blog posts
+      </div>
     );
   }
 }
