9a47cc4a7bf8b297f614890a120ec2e12d0ee328
yenly <yencodes@gmail.com>
2016-11-16 15:40:49 -0800

    Install redux-form, create new action creator createPost, then use redux-form to handleSubmit using createPost.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
+    // const handleSubmit = this.props.handleSubmit;
+    // es6 shortcut syntax for above code
+    // const title = this.props.fields.title is same as
+    // fields: { title, categories, content } --> es6 syntax
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
     return (
-      <div>Create Form</div>
+      <form onSubmit={handleSubmit(this.props.createPost)}> // handleSubmit calls action creator
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea type="text" className="form-control" {...content} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps,
+// 3rd is mapDispatchToProps (action creator)
+
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
