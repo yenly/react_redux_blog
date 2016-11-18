f549d93e5fb5e7b5ff0d234d89e7c40e029a31f5
yenly <yencodes@gmail.com>
2016-11-18 03:02:09 -0800

    Add validation to form and cancel button to return user to blog list.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
   render() {
@@ -11,27 +12,62 @@ class PostsNew extends Component {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}> // handleSubmit calls action creator
+      // handleSubmit calls action creator
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
+          <div className="text-danger">
+            {/* <-- example of JSX comment syntax
+              .touched is prop from reduxForm - means if user touch this form
+              below is example of ternary expression
+              if title.touched is true, return title.error else return ''
+            */}
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
+          <div className="text-danger">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea type="text" className="form-control" {...content} />
+          <div className="text-danger">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if(!values.title) {
+    // key needs to match field names
+    errors.title = 'Enter a username';
+  }
+
+  if(!values.categories) {
+    errors.categories = 'Enter categories';
+  }
+
+  if(!values.content) {
+    errors.content = 'Enter some content';
+  }
+
+  return errors;
 }
 
 // connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
@@ -40,5 +76,6 @@ class PostsNew extends Component {
 
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
